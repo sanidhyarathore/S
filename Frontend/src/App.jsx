86785e9dfd7ca69329a3e51efad72df8d2de825d
Sanidhya Rathore <sanidhya.rathore263@gmail.com>
2024-11-05 00:12:56 +0530

    register page added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return <div>
-    <Login />
+    <Register />
   </div>;
 }
 
--- Frontend/src/pages/register/Register.jsx
+++ Frontend/src/pages/register/Register.jsx
@@ -1,8 +1,29 @@
 import './register.scss'
 
+
 const Register = () => {
     return (
-        <div className='login'>Login</div>
+        <div className='register'>
+            <div className="card">
+                <div className="left">
+                    <h1>Register</h1>
+                    <form >
+                        <input type="text" placeholder='Name' />
+                        <input type="text" placeholder='Username' />
+                        <input type="text" placeholder='Email' />
+                        <input type="password" placeholder='Password' />
+                        <button>Register</button>
+                    </form>
+                </div>
+                <div className="right">
+                    <h1>Sanu Social.</h1>
+                    <p>
+                        This is a register page where any new user can come up and register for social media.
+                    </p>
+                    <span>Do you have an account?</span>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Register
